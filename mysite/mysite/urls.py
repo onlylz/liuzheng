85cda72e863a4ad034590ad5c63292e81ebcccf7
83042609@qq.com <Lz123456>
2016-12-24 23:50:11 +0800

    1

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.first_page),
     url(r'^west/', include('west.urls')),
 ]
