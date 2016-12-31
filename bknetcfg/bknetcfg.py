f87ec21c83a56288cf4d4cf54c6ac36cd3285fbd
liuzheng <83042609@qq.com>
2016-12-31 08:34:21 +0800

    1

--- bknetcfg/bknetcfg.py
+++ bknetcfg/bknetcfg.py
@@ -1,58 +1,261 @@
-# coding: utf8
+#!/usr/local/bin/python
+# encoding=utf-8
+
 import sys
+import datetime
 import telnetlib
-import time
+from ftplib import FTP
 import pymysql
-import os
-import shutil
 import logging
-import paramiko
+from mylogging import setup_logging
 
-def bk_telnet_h3c_1(name, ip, area):
-    host = {}
-    host['name'] = name.encode(encoding="utf-8")
-    host['ip'] = ip.encode(encoding="utf-8")
-    host['user'] = b"liuzheng"
-    host['password'] = b"jjyhncfh"
-    host['tftp'] = b"tftp 99.99.11.11 put startup.cfg config.txt"
+FTP_SERVER = {'ip': '192.90.33.235',
+              'port': 21,
+              'user': 'bknetcfg',
+              'passwd': 'bknetcfg',
+              'bak_dir': '/'}
+
+DB_SERVER = {'host': '192.90.33.212',
+             'port': 3306,
+             'user': 'chknet',
+             'password': 'chknet',
+             'db': 'chknet',
+             'charset': 'utf8mb4',
+             'cursorclass': pymysql.cursors.DictCursor}
+
+
+def mk_bak_dir_on_ftp(ftp_server, bak_dir):
+    #print(sys._getframe().f_code.co_name)
+
+    logger.info("Begin to create directory on FTP server for backup...")
+    ftp = FTP()
     try:
-        tn = telnetlib.Telnet(host['ip'])
-        tn.set_debuglevel(2)
-        try:
-            tn.read_until(b"(?i)Psername:", timeout=10)
-        except EOFError:
-            logging.error("time out")
-        tn.write(host['user']+b"\n")
-        tn.read_until(b"Password:", timeout=2)
-        tn.write(host['password']+b"\n")
-        tn.read_until(b">", timeout=2)
-        tn.write(host['tftp']+b"\n")
-        tn.read_until(b"successfully", timeout=6)
-        # movefile_txt(name, ip, area)
-    except TimeoutError:
-        logging.warning(name + "(" + ip + ") TimeoutError: [WinError 10060] 由于连接方在一段时间后没有正确答复或连接的主机没有反应，连接尝试失败。")
-    except FileNotFoundError:
-        logging.error(name + "(" + ip + ") FileNotFoundError: 未成功备份startup.cfg！")
-    except ConnectionResetError:
-        logging.warning(name + "(" + ip + ") ConnectionResetError: [WinError 10054] 远程主机强迫关闭了一个现有的连接。")
+        ftp.connect(ftp_server['ip'], ftp_server['port'])
     except:
-        logging.error(name + "(" + ip + ") 发生未知错误！")
+        logger.error("Can not connect FTP server " + ftp_server['ip'] + "!")
+        exit(3)
+    try:
+        ftp.login(ftp_server['user'], ftp_server['passwd'])
+    except:
+        logger.error("Can not login FTP server " + ftp_server['ip'] + "!")
+        exit(3)
+
+    try:
+        ftp.mkd(bak_dir)
+    except:
+        logger.error("Can not create directory " + bak_dir + " on FTP server " + ftp_server['ip'] + "!")
+        ftp.quit()
+        exit(3)
+    logger.info("Create directory " + bak_dir + " on FTP server " + ftp_server['ip'] + " success!")
+    ftp.quit()
+    return
+
+def get_devices(db_server):
+    #print(sys._getframe().f_code.co_name)
+    logger.info("Begin to get network device's parameter from DB server...")
+    try:
+        conn = pymysql.connect(**db_server)
+    except:
+        logger.error("Can not connect to DB server " + DB_SERVER['host'] + "!")
+    result=[]
+    try:
+        with conn.cursor() as cursor:
+            sql = 'SELECT vendor, sw_ver, name, hostname, ipaddr, user, passwd, supasswd FROM cn_device WHERE bak_cfg = % s ORDER BY hostname'
+            cursor.execute(sql, '1')
+            result = cursor.fetchall()
+
+        # 没有设置默认自动提交，需要主动提交，以保存所执行的语句
+        #conn.commit()
+        #
+    except:
+        logger.error("Can not get network device's parameter from DB server!")
+    finally:
+        conn.close()
+        logger.info("Get " + str(len(result)) + " network device's parameter from DB server.")
+        return result
+
+def bak_H3C_Sv5_cfg(device, ftp_server):
+    #适用于S3100、S3100v2
+    #print(sys._getframe().f_code.co_name)
+    logger.info("Begin to backup " + device['name'] + "(" + device['ipaddr'] + ")'s config ...")
+    try:
+        tn = telnetlib.Telnet(device['ipaddr'])
+        tn.set_debuglevel(2)
+    except:
+        logger.error("Can not connect to " + device['name'] + "(" + device['ipaddr'] + ")!")
+        logger.error("Backup " + device['name'] + "(" + device['ipaddr'] + ")'s config failure!")
+        return 0
+
+    try:
+        tn.read_until(b"Username:")
+        tn.write(device['user'].encode('utf-8') + b"\n")
+        tn.read_until(b"Password:")
+        tn.write(device['passwd'].encode('utf-8') + b"\n")
+        tn.read_until(b">")
+        tn.write(b"su \n")
+        tn.read_until(b"Password:")
+        tn.write(device['supasswd'].encode('utf-8') + b"\n")
+        tn.read_until(b">")
+        tn.write(b"ftp " + ftp_server['ip'].encode('utf-8') + b"\n")
+        tn.read_until(b"):")
+        tn.write(ftp_server['user'].encode('utf-8') + b"\n")
+        tn.read_until(b"Password:")
+        tn.write(ftp_server['passwd'].encode('utf-8') + b"\n")
+        tn.read_until(b"[ftp]")
+        tn.write(b"cd " + ftp_server['bak_dir'].encode('utf-8') + b"\n")
+        tn.read_until(b"[ftp]")
+        tn.write(b"put config.cfg " + device['hostname'].encode('utf-8') + b".cfg\n")
+        tn.read_until(b"[ftp]")
+        tn.write(b"quit\n")
+        tn.read_until(b">")
+        tn.write(b"quit\n")
+        tn.close()
+        logger.info("Backup " + device['name'] + "(" + device['ipaddr'] + ")'s config successful.")
+        return 1
+    except:
+        logger.error("Encounter errors when backuping " + device['name'])
+        logger.error("Backup " + device['name'] + "(" + device['ipaddr'] + ")'s config failure!")
+        return 0
+
+def bak_H3C_Rv5_cfg(device, ftp_server):
+    #适用S7507、R20XX、R30XX
+    #print(sys._getframe().f_code.co_name)
+
+    logger.info("Begin to backup " + device['name'] + "(" + device['ipaddr'] + ")'s config ...")
+    try:
+        tn = telnetlib.Telnet(device['ipaddr'])
+        tn.set_debuglevel(2)
+    except:
+        logger.error("Can not connect to " + device['name'] + "(" + device['ipaddr'] + ")!")
+        logger.error("Backup " + device['name'] + "(" + device['ipaddr'] + ")'s config failure!")
+        return 0
+
+    try:
+        tn.read_until(b"Username:")
+        tn.write(device['user'].encode('utf-8') + b"\n")
+        tn.read_until(b"Password:")
+        tn.write(device['passwd'].encode('utf-8') + b"\n")
+        tn.read_until(b">")
+        tn.write(b"su \n")
+        tn.read_until(b"Password:")
+        tn.write(device['supasswd'].encode('utf-8') + b"\n")
+        tn.read_until(b">")
+        tn.write(b"ftp " + ftp_server['ip'].encode('utf-8') + b"\n")
+        tn.read_until(b"):")
+        tn.write(ftp_server['user'].encode('utf-8') + b"\n")
+        tn.read_until(b"Password:")
+        tn.write(ftp_server['passwd'].encode('utf-8') + b"\n")
+        tn.read_until(b"[ftp]")
+        tn.write(b"cd " + ftp_server['bak_dir'].encode('utf-8') + b"\n")
+        tn.read_until(b"[ftp]")
+        tn.write(b"put startup.cfg " + device['hostname'].encode('utf-8') + b".cfg\n")
+        tn.read_until(b"[ftp]")
+        tn.write(b"quit\n")
+        tn.read_until(b">")
+        tn.write(b"quit\n")
+        tn.close()
+        logger.info("Backup " + device['name'] + "(" + device['ipaddr'] + ")'s config successful.")
+        return 1
+    except:
+        logger.error("Encounter error when backuping " + device['name'])
+        logger.error("Backup " + device['name'] + "(" + device['ipaddr'] + ")'s config failure!")
+        return 0
+
+def bak_H3C_Rv7_cfg(device, ftp_server):
+    # 适用3600-28、3600-51
+    #print(sys._getframe().f_code.co_name)
+
+    logger.info("Begin to backup " + device['name'] + "(" + device['ipaddr'] + ")'s config ...")
+    try:
+        tn = telnetlib.Telnet(device['ipaddr'])
+        tn.set_debuglevel(2)
+    except:
+        logger.error("Can not connect to " + device['name'] + "(" + device['ipaddr'] + ")!")
+        logger.error("Backup " + device['name'] + "(" + device['ipaddr'] + ")'s config failure!")
+        return 0
+
+    try:
+        tn.read_until(b"login:")
+        tn.write(device['user'].encode('utf-8') + b"\n")
+        tn.read_until(b"Password:")
+        tn.write(device['passwd'].encode('utf-8') + b"\n")
+        tn.read_until(b">")
+        tn.write(b"su \n")
+        tn.read_until(b"Password:")
+        tn.write(device['supasswd'].encode('utf-8') + b"\n")
+        tn.read_until(b">")
+        tn.write(b"ftp " + ftp_server['ip'].encode('utf-8') + b"\n")
+        tn.read_until(b"):")
+        tn.write(ftp_server['user'].encode('utf-8') + b"\n")
+        tn.read_until(b"Password:")
+        tn.write(ftp_server['passwd'].encode('utf-8') + b"\n")
+        tn.read_until(b"ftp>")
+        tn.write(b"cd " + ftp_server['bak_dir'].encode('utf-8') + b"\n")
+        tn.read_until(b"ftp>")
+        tn.write(b"put startup.cfg " + device['hostname'].encode('utf-8') + b".cfg\n")
+        tn.read_until(b"ftp>")
+        tn.write(b"quit\n")
+        tn.read_until(b">")
+        tn.write(b"quit\n")
+        tn.close()
+        logger.info("Backup " + device['name'] + "(" + device['ipaddr'] + ")'s config successful.")
+        return 1
+    except:
+        logger.error("Encounter error when backuping " + device['name'])
+        logger.error("Backup " + device['name'] + "(" + device['ipaddr'] + ")'s config failure!")
+        return 0
+
+def bak_CSC_v12_cfg(device, ftp_server):
+    # 适用3800,2900
+    #print(sys._getframe().f_code.co_name)
+
+    logger.info("Begin to backup " + device['name'] + "(" + device['ipaddr'] + ")'s config ...")
+    try:
+        tn = telnetlib.Telnet(device['ipaddr'])
+        tn.set_debuglevel(2)
+    except:
+        logger.error("Can not connect to " + device['name'] + "(" + device['ipaddr'] + "）!")
+        logger.error("Backup " + device['name'] + "(" + device['ipaddr'] + ")'s config failure!")
+        return 0
+
+    try:
+        tn.read_until(b"Username:")
+        tn.write(device['user'].encode('utf-8') + b"\n")
+        tn.read_until(b"Password:")
+        tn.write(device['passwd'].encode('utf-8') + b"\n")
+        tn.read_until(b">")
+        tn.write(b"en\n")
+        tn.read_until(b"Password:")
+        tn.write(device['supasswd'].encode('utf-8') + b"\n")
+        tn.read_until(b"#")
+        tn.write(b"copy start ftp\n")
+        tn.read_until(b"]?")
+        tn.write(ftp_server['ip'].encode('utf-8') + b"\n")
+        tn.read_until(b"]?")
+        tn.write(ftp_server['bak_dir'].encode('utf-8') +b"/" + device['hostname'].encode('utf-8') + b".cfg\n")
+        tn.read_until(b"#")
+        tn.write(b"exit\n")
+        tn.close()
+        logger.info("Backup " + device['name'] + "(" + device['ipaddr'] + ")'s config successful.")
+        return 1
+    except:
+        logger.error("Encounter error when backuping " + device['name'])
+        logger.error("Backup " + device['name'] + "(" + device['ipaddr'] + ")'s config failure!")
+        return 0
 
 
-file_year = time.strftime("%Y", time.localtime())
-file_mon = time.strftime("%m", time.localtime())
-file_mday = time.strftime("%d", time.localtime())
-file_time = time.strftime("%Y%m%d", time.localtime())
-backup_dir = os.getcwd() + "\\备份路径\\"
-tftp_dir = os.getcwd() + "\\TFTP\\Root\\"
-logging_dir = os.getcwd() + "\\备份路径\\备份程序执行日志"
-logging_name = "log_" + file_time + ".txt"
-if not os.path.isdir(logging_dir):
-    os.makedirs(logging_dir)
-logging.basicConfig(filename = os.path.join(logging_dir, logging_name), level = logging.INFO, filemode = "a", format = "%(asctime)s - %(levelname)s: %(message)s")
-#logging.debug("debug")
-#logging.info("info")
-#logging.warning("warn")
-#logging.error("error")
-bk_telnet_h3c_1("11F","99.99.11.1","11")
-logging.shutdown()
+
+if __name__ == '__main__':
+
+    setup_logging()
+    logger = logging.getLogger(__name__)
+    bat_no = datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
+    logger.info("***Begin to backup network device's config to FTP...")
+    ftp_server = mk_bak_dir_on_ftp(FTP_SERVER, bat_no)
+    FTP_SERVER['bak_dir'] = bat_no
+    devices = get_devices(DB_SERVER)
+    backed_devices = 0
+    for device in devices:
+        backed_devices = backed_devices + eval("bak_" + device['vendor'] + "_" + device['sw_ver'] + "_cfg")(device, FTP_SERVER)
+    logger.info("There are " + str(backed_devices) + " of " + str(len(devices)) + " devices backuped to ftp://" + \
+          FTP_SERVER['ip'] + '/' + FTP_SERVER['bak_dir'] + ".")
--- bknetcfg/test2.py
+++ bknetcfg/test2.py
@@ -10,7 +10,7 @@ FTP_SERVER = {'ip': '192.90.33.235',
               'port': 21,
               'user': 'bknetcfg',
               'passwd': 'bknetcfg',
-              'bak_dir': ''}
+              'bak_dir': '/'}
 
 DB_SERVER = {'host': '192.90.33.212',
              'port': 3306,
@@ -19,8 +19,6 @@ DB_SERVER = {'host': '192.90.33.212',
              'db': 'chknet',
              'charset': 'utf8mb4',
              'cursorclass': pymysql.cursors.DictCursor}
-
-
 
 
 def mk_bak_dir_on_ftp(ftp_server, bak_dir):
@@ -33,7 +31,7 @@ def mk_bak_dir_on_ftp(ftp_server, bak_dir):
         print("Can not connect server " + ftp_server['ip'] + "!")
         exit(3)
     try:
-        ftp.login(ftp_server['user'],ftp_server['passwd'])
+        ftp.login(ftp_server['user'], ftp_server['passwd'])
     except:
         print("Can not login server " + ftp_server['ip'] + "!")
         exit(3)
@@ -45,11 +43,10 @@ def mk_bak_dir_on_ftp(ftp_server, bak_dir):
         ftp.quit()
         exit(3)
     print(ftp.dir())
-    print("Create directory " + ftp_server + " on server " + ftp_server['ip'] + "success!")
+    print("Create directory " + bak_dir + " on server " + ftp_server['ip'] + " success!")
     ftp.quit()
 
     return
-
 
 def get_devices(db_server):
     print(sys._getframe().f_code.co_name)
@@ -57,8 +54,10 @@ def get_devices(db_server):
     conn = pymysql.connect(**db_server)
     try:
         with conn.cursor() as cursor:
-            sql = 'SELECT vendor, type, name, ipaddr, user, passwd, supasswd FROM cn_device'
-            cursor.execute(sql)
+            # sql = 'SELECT vendor, sw_ver, name, ipaddr, user, passwd, supasswd FROM cn_device'
+            # cursor.execute(sql)
+            sql = 'SELECT vendor, sw_ver, name, hostname, ipaddr, user, passwd, supasswd FROM cn_device WHERE bak_cfg = % s ORDER BY hostname'
+            cursor.execute(sql, '1')
             result = cursor.fetchall()
 
         # 没有设置默认自动提交，需要主动提交，以保存所执行的语句
@@ -67,53 +66,187 @@ def get_devices(db_server):
         conn.close()
         return result
 
-def bak_csc_r38_cfg(device, ftp_server):
-    print("bak_csc_r38_cfg: " + device['name'], device['ipaddr'], device['user'], device['passwd'], device['supasswd'])
-
-def bak_h3c_s75_cfg(device, ftp_server):
-    print("bak_h3c_s75_cfg: " + device['name'], device['ipaddr'], device['user'], device['passwd'], device['supasswd'])
-
-def bak_h3c_s31_cfg(device, ftp_server):
-    print("Begin to backup " + device['name'] + "(" + device['ipaddr'] + ")'s config to ftp://" \
-          + ftp_server['ip'] + '/' + ftp_server['bak_dir'] + '...')
+def bak_H3C_Sv5_cfg(device, ftp_server):
+    #适用于S3100、S3100v2
+    print(sys._getframe().f_code.co_name)
+    print("Begin to backup " + device['name'] + "(" + device['ipaddr'] + ")'s config ...")
     try:
-        tn = telnetlib.Telnet(device['ip'])
+        tn = telnetlib.Telnet(device['ipaddr'])
         tn.set_debuglevel(2)
-        try:
-            tn.read_until(b"(?i)Username:", timeout=10)
-        except EOFError:
-            print("time out")
-        tn.write(device['user']+b"\n")
-        tn.read_until(b"Password:", timeout=2)
-        tn.write(device['passwd']+b"\n")
-        tn.read_until(b">", timeout=2)
-        #tn.write(host['tftp']+b"\n")
-        #tn.read_until(b"successfully", timeout=6)
-        # movefile_txt(name, ip, area)
-    except TimeoutError:
-        print(device['name'] + "(" + device['ip'] + ") TimeoutError: [WinError 10060] 由于连接方在一段时间后没有正确答复或连接的主机没有反应，连接尝试失败。")
-    except FileNotFoundError:
-        print(device['name'] + "(" + device['ip'] + ") FileNotFoundError: 未成功备份startup.cfg！")
-    except ConnectionResetError:
-        print(device['name'] + "(" + device['ip'] + ") ConnectionResetError: [WinError 10054] 远程主机强迫关闭了一个现有的连接。")
     except:
-        print(device['name'] + "(" + device['ip'] + ") 发生未知错误！")
+        print(device['name'] + "(" + device['ipaddr'] + "）无法连接")
+        print("Backup " + device['name'] + "(" + device['ipaddr'] + ")'s config failure")
+        return 0
 
-    print("Backup " + device['name'] + "(" + device['ipaddr'] + ")'s config to ftp://" \
-          + ftp_server['ip'] + '/' + ftp_server['bak_dir'] + 'successful.')
-'''def bak_dev_cfg(device):
-    print(device['vendor'], device['type'], device['name'], device['ipaddr'], device['user'], device['passwd'], device['supasswd'])
-    #eval("bak_" + device['vendor'] + "_" + device['type'] + "_cfg")\
-    # (device['name'], device['ipaddr'], device['user'], device['passwd'], device['supasswd'])
-    #eval("bak_" + device['vendor'] + "_" + device['type'] + "_cfg")(device)
-'''
+    try:
+        tn.read_until(b"Username:")
+        tn.write(device['user'].encode('utf-8') + b"\n")
+        tn.read_until(b"Password:")
+        tn.write(device['passwd'].encode('utf-8') + b"\n")
+        tn.read_until(b">")
+        tn.write(b"su \n")
+        tn.read_until(b"Password:")
+        tn.write(device['supasswd'].encode('utf-8') + b"\n")
+        tn.read_until(b">")
+        tn.write(b"ftp " + ftp_server['ip'].encode('utf-8') + b"\n")
+        tn.read_until(b"):")
+        tn.write(ftp_server['user'].encode('utf-8') + b"\n")
+        tn.read_until(b"Password:")
+        tn.write(ftp_server['passwd'].encode('utf-8') + b"\n")
+        tn.read_until(b"[ftp]")
+        tn.write(b"cd " + ftp_server['bak_dir'].encode('utf-8') + b"\n")
+        tn.read_until(b"[ftp]")
+        tn.write(b"put config.cfg " + device['hostname'].encode('utf-8') + b".cfg\n")
+        tn.read_until(b"[ftp]")
+        tn.write(b"quit\n")
+        tn.read_until(b">")
+        tn.write(b"quit\n")
+        tn.close()
+        print("Backup " + device['name'] + "(" + device['ipaddr'] + ")'s config successful.")
+        return 1
+    except:
+        print("Encounter error when backuping " + device['name'])
+        print("Backup " + device['name'] + "(" + device['ipaddr'] + ")'s config failure")
+        return 0
+
+def bak_H3C_Rv5_cfg(device, ftp_server):
+    #适用S7507、R20XX、R30XX
+    print(sys._getframe().f_code.co_name)
+
+    print("Begin to backup " + device['name'] + "(" + device['ipaddr'] + ")'s config ...")
+    try:
+        tn = telnetlib.Telnet(device['ipaddr'])
+        tn.set_debuglevel(2)
+    except:
+        print(device['name'] + "(" + device['ipaddr'] + "）无法连接")
+        print("Backup " + device['name'] + "(" + device['ipaddr'] + ")'s config failure")
+        return 0
+
+    try:
+        tn.read_until(b"Username:")
+        tn.write(device['user'].encode('utf-8') + b"\n")
+        tn.read_until(b"Password:")
+        tn.write(device['passwd'].encode('utf-8') + b"\n")
+        tn.read_until(b">")
+        tn.write(b"su \n")
+        tn.read_until(b"Password:")
+        tn.write(device['supasswd'].encode('utf-8') + b"\n")
+        tn.read_until(b">")
+        tn.write(b"ftp " + ftp_server['ip'].encode('utf-8') + b"\n")
+        tn.read_until(b"):")
+        tn.write(ftp_server['user'].encode('utf-8') + b"\n")
+        tn.read_until(b"Password:")
+        tn.write(ftp_server['passwd'].encode('utf-8') + b"\n")
+        tn.read_until(b"[ftp]")
+        tn.write(b"cd " + ftp_server['bak_dir'].encode('utf-8') + b"\n")
+        tn.read_until(b"[ftp]")
+        tn.write(b"put startup.cfg " + device['hostname'].encode('utf-8') + b".cfg\n")
+        tn.read_until(b"[ftp]")
+        tn.write(b"quit\n")
+        tn.read_until(b">")
+        tn.write(b"quit\n")
+        tn.close()
+        print("Backup " + device['name'] + "(" + device['ipaddr'] + ")'s config successful.")
+        return 1
+    except:
+        print("Encounter error when backuping " + device['name'])
+        print("Backup " + device['name'] + "(" + device['ipaddr'] + ")'s config failure")
+        return 0
+
+def bak_H3C_Rv7_cfg(device, ftp_server):
+    # 适用3600-28、3600-51
+    print(sys._getframe().f_code.co_name)
+
+    print("Begin to backup " + device['name'] + "(" + device['ipaddr'] + ")'s config ...")
+    try:
+        tn = telnetlib.Telnet(device['ipaddr'])
+        tn.set_debuglevel(2)
+    except:
+        print(device['name'] + "(" + device['ipaddr'] + "）无法连接")
+        print("Backup " + device['name'] + "(" + device['ipaddr'] + ")'s config failure")
+        return 0
+
+    try:
+        tn.read_until(b"login:")
+        tn.write(device['user'].encode('utf-8') + b"\n")
+        tn.read_until(b"Password:")
+        tn.write(device['passwd'].encode('utf-8') + b"\n")
+        tn.read_until(b">")
+        tn.write(b"su \n")
+        tn.read_until(b"Password:")
+        tn.write(device['supasswd'].encode('utf-8') + b"\n")
+        tn.read_until(b">")
+        tn.write(b"ftp " + ftp_server['ip'].encode('utf-8') + b"\n")
+        tn.read_until(b"):")
+        tn.write(ftp_server['user'].encode('utf-8') + b"\n")
+        tn.read_until(b"Password:")
+        tn.write(ftp_server['passwd'].encode('utf-8') + b"\n")
+        tn.read_until(b"ftp>")
+        tn.write(b"cd " + ftp_server['bak_dir'].encode('utf-8') + b"\n")
+        tn.read_until(b"ftp>")
+        tn.write(b"put startup.cfg " + device['hostname'].encode('utf-8') + b".cfg\n")
+        tn.read_until(b"ftp>")
+        tn.write(b"quit\n")
+        tn.read_until(b">")
+        tn.write(b"quit\n")
+        tn.close()
+        print("Backup " + device['name'] + "(" + device['ipaddr'] + ")'s config successful.")
+        return 1
+    except:
+        print("Encounter error when backuping " + device['name'])
+        print("Backup " + device['name'] + "(" + device['ipaddr'] + ")'s config failure")
+        return 0
+
+def bak_CSC_v12_cfg(device, ftp_server):
+    # 适用3800,2900
+    print(sys._getframe().f_code.co_name)
+
+    print("Begin to backup " + device['name'] + "(" + device['ipaddr'] + ")'s config ...")
+    try:
+        tn = telnetlib.Telnet(device['ipaddr'])
+        tn.set_debuglevel(2)
+    except:
+        print(device['name'] + "(" + device['ipaddr'] + "）无法连接")
+        print("Backup " + device['name'] + "(" + device['ipaddr'] + ")'s config failure")
+        return 0
+
+    try:
+        tn.read_until(b"Username:")
+        tn.write(device['user'].encode('utf-8') + b"\n")
+        tn.read_until(b"Password:")
+        tn.write(device['passwd'].encode('utf-8') + b"\n")
+        tn.read_until(b">")
+        tn.write(b"en\n")
+        tn.read_until(b"Password:")
+        tn.write(device['supasswd'].encode('utf-8') + b"\n")
+        tn.read_until(b"#")
+        tn.write(b"copy start ftp\n")
+        tn.read_until(b"]?")
+        tn.write(ftp_server['ip'].encode('utf-8') + b"\n")
+        tn.read_until(b"]?")
+        tn.write(ftp_server['bak_dir'].encode('utf-8') +b"/" + device['hostname'].encode('utf-8') + b".cfg\n")
+        tn.read_until(b"#")
+        tn.write(b"exit\n")
+        tn.close()
+        print("Backup " + device['name'] + "(" + device['ipaddr'] + ")'s config successful.")
+        return 1
+    except:
+        print("Encounter error when backuping " + device['name'])
+        print("Backup " + device['name'] + "(" + device['ipaddr'] + ")'s config failure")
+        return 0
+
+
+
 if __name__ == '__main__':
 
     bat_no = datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
-
-    #ftp_server = mk_ftp_dir(FTP_SERVER, bat_no)
+    print("***Begin to backup network device's config to FTP***")
+    ftp_server = mk_bak_dir_on_ftp(FTP_SERVER, bat_no)
     FTP_SERVER['bak_dir'] = bat_no
     devices = get_devices(DB_SERVER)
+    backed_devices = 0
     for device in devices:
-        #bak_dev_cfg(device)
-        #eval("bak_" + device['vendor'] + "_" + device['type'] + "_cfg")(device,FTP_SERVER)
+        # bak_dev_cfg(device)
+        backed_devices = backed_devices + eval("bak_" + device['vendor'] + "_" + device['sw_ver'] + "_cfg")(device, FTP_SERVER)
+    print("There are " + str(backed_devices) + " of " + str(len(devices)) + " devices backuped to ftp://" + \
+          FTP_SERVER['ip'] + '/' + FTP_SERVER['bak_dir'] + ".")
